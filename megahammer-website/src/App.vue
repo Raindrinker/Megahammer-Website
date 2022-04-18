007378b42581e0ecb5866a496cc84fccf675b383
Ferran Ruiz <ferriruiz3@gmail.com>
2022-04-18 21:56:35 +0200

    Initial setup

---
+++ megahammer-website/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="main">
+    <img src="./assets/vectormonke.png" class="logo">
+    <div class="text">UNDER CONSTRUCTION</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+  margin: 0;
+  height: 100vh;
+  overflow: hidden;
+
+  display: flex;
+  flex-direction: column;
+}
+
+body {
+  margin: 0;
+  background-color: black;
+}
+
+.main {
+  flex-grow: 1;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding-top: 128px;
+}
+
+.logo {
+  max-width: 30%;
+}
+
+.text {
+  color: #AAAAAA;
+  font-size: 24px;
+}
+</style>
